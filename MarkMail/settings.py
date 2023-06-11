22c7e061901e328e159e3298485a7efa2736a1aa
M <50911439+mPenTests@users.noreply.github.com>
2023-06-11 09:50:00 +0200

    Update settings.py

--- MarkMail/settings.py
+++ MarkMail/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3567951e3751b062741ca5bb2433c6d603dff66ab120e4b59b6e58b7d15478d4'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
